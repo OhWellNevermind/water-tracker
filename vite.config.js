6800a5688f79def01d3fdd43cbb6816398fa2168
OhWellNevermind <52574266+OhWellNevermind@users.noreply.github.com>
2023-12-21 00:06:50 +0200

    added structure for repo

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
-    extensions: [".jsx", ".json", ".css"],
+    extensions: [".jsx", ".json", ".css", ".js"],
   },
   plugins: [react()],
 });
