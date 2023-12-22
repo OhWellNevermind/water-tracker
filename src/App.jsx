62146e20452dfbf9d4b31c66c36abdebb9c07c8f
SophiaTretiakova <122561236+SophiaTretiakova@users.noreply.github.com>
2023-12-22 22:33:14 +0200

    sign up for mobile

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
-import { Home } from "./pages/Home/Home";
+// import { Home } from "./pages/Home/Home";
+import { SignUp } from "./pages/Home/SignUp/SignUp";
 
 export const App = () => {
   return (
     <Routes>
-      <Route index element={<Home />} />
+      {/* <Route index element={<Home />} /> */}
+      <Route index element={<SignUp />} />
     </Routes>
   );
 };
