57eeac53b867a24a07931933305d8af08899612b
OhWellNevermind <52574266+OhWellNevermind@users.noreply.github.com>
2023-12-24 17:58:45 +0200

    cleaned Home component

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,24 +1,3 @@
-import { useState } from "react";
-import BaseModalWrap from "../../components/modals/ModalWrap/ModalWrap";
-import { colors } from "../../constants";
-import DailyNorma from "../../components/modals/DailyNorma/DailyNorma";
-
 export const Home = () => {
-  const [open, setOpen] = useState(true);
-  return (
-    <>
-      <div style={{ fontWeight: 500, fontSize: 32 }}>Home</div>
-      <button
-        style={{ width: "50px", height: "30px", backgroundColor: colors.BLUE }}
-        onClick={() => setOpen(true)}
-      >
-        Open
-      </button>
-      {open && (
-        <BaseModalWrap onClose={() => setOpen(false)}>
-          <DailyNorma onClose={() => setOpen(false)}/>
-        </BaseModalWrap>
-      )}
-    </>
-  );
+  return <></>;
 };
