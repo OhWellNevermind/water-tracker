7140a7bdd45c5c8c5333abde1247884bdf2a9b76
OhWellNevermind <52574266+OhWellNevermind@users.noreply.github.com>
2023-12-25 15:00:41 +0200

    Modal ready

--- src/components/modals/UserModal/UserModal.styled.js
+++ src/components/modals/UserModal/UserModal.styled.js
@@ -13,6 +13,13 @@ export const ModalContainer = styled.div`
     width: 656px;
     padding: 32px 24px;
   }
+
+  @media (min-width: 1439px) {
+    width: 960px;
+    padding: 32px 24px;
+    height: fit-content;
+    overflow: hidden;
+  }
 `;
 
 export const Title = styled.h2`
@@ -21,6 +28,15 @@ export const Title = styled.h2`
   line-height: 1.23;
   color: ${colors.GRAY};
   margin-bottom: 24px;
+`;
+
+export const TitleContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const CloseIconContainer = styled.div`
+  cursor: pointer;
 `;
 
 export const Container = styled.div`
@@ -96,6 +112,10 @@ export const GenderContainer = styled.div`
   align-items: center;
   gap: 24px;
   margin-bottom: 24px;
+
+  @media screen and (min-width: 1439px) {
+    margin-bottom: 58px;
+  }
 `;
 
 export const GenderInputLabel = styled.label`
@@ -138,10 +158,10 @@ export const RadioButton = styled.div`
     border-radius: 50%;
     width: 7px;
     height: 7px;
-    /* position: absolute;
+    position: absolute;
     top: 50%;
     left: 50%;
-    transform: translate(-50%, -50%); */
+    transform: translate(-50%, -50%);
     background-color: ${colors.BLUE};
   }
 `;
@@ -149,6 +169,14 @@ export const RadioButton = styled.div`
 export const Form = styled.form`
   display: flex;
   flex-direction: column;
+`;
+
+export const InputsContainer = styled.div`
+  @media screen and (min-width: 1439px) {
+    display: flex;
+    flex-direction: row;
+    gap: 24px;
+  }
 `;
 
 export const SubmitButton = styled.button`
@@ -179,6 +207,11 @@ export const SubmitButton = styled.button`
     width: 100px;
     align-self: flex-end;
   }
+  @media (min-width: 1439px) {
+    padding: 10px 0;
+    width: 160px;
+    align-self: flex-end;
+  }
 `;
 
 export const InputContainer = styled.div`
@@ -200,10 +233,22 @@ export const TextInput = styled.input`
   &::placeholder {
     color: ${colors.PROVINCIAL_PINK};
   }
-
+  outline: none;
   @media (min-width: 767px) {
     width: 372px;
   }
+  &.error {
+    border-color: ${colors.RED};
+    color: ${colors.RED};
+  }
+`;
+
+export const ErrorMessage = styled.p`
+  color: ${colors.RED};
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 1.28; /* 128.571% */
 `;
 
 export const IconInputContainer = styled.div`
@@ -227,6 +272,11 @@ export const InputWithIcon = styled.input`
   border-radius: 6px;
   padding: 12px 0 12px 10px;
   color: ${colors.BLUE};
+  outline: none;
+  &.error {
+    border-color: ${colors.RED};
+    color: ${colors.RED};
+  }
   &::placeholder {
     color: ${colors.PROVINCIAL_PINK};
   }
