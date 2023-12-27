8c7ce9aeb0302763720474fa4507faa688e70cb7
Teslyak <124871929+Teslyak@users.noreply.github.com>
2023-12-27 19:04:22 +0200

    fix input fix button save

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import { Routes, Route } from "react-router-dom";
-import WelcomePage  from './pages/WelcomePage/WelcomePage'
+import WelcomePage from "./pages/WelcomePage/WelcomePage";
 import { GlobalStyles } from "./components/GlobalStyles";
 import { SharedLayout } from "./components/SharedLayout";
-
+import { Home } from "./pages/Home/Home";
 export const App = () => {
   return (
     <>
       <Routes>
         <Route path="/" element={<SharedLayout />}>
           <Route index element={<WelcomePage />} />
+          <Route path="home" element={<Home />} />
         </Route>
       </Routes>
       <GlobalStyles />
