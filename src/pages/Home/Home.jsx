aa7dee701daf051bf21d8b7de0a5066a539543bd
OhWellNevermind <52574266+OhWellNevermind@users.noreply.github.com>
2023-12-28 22:41:48 +0200

    remove div and import

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -8,25 +8,9 @@ import {
   DailyNormaAndWaterRatioPanel,
 } from "./Home.styled";
 
-import { useState } from "react";
-import { colors } from "../../constants";
-
-import { Header } from "../../components/Header/Header";
-import { UserModal } from "../../components/modals/UserModal/UserModal";
-
 export const Home = () => {
-  const [open, setOpen] = useState(false);
   return (
     <>
-      <Header />
-      <div style={{ fontWeight: 500, fontSize: 32 }}>Home</div>
-      <button
-        style={{ width: "50px", height: "30px", backgroundColor: colors.BLUE }}
-        onClick={() => setOpen(true)}
-      >
-        Open
-      </button>
-      {open && <UserModal setIsOpen={setOpen} />}
       <ContainerHome>
         <DailyNormaAndWaterRatioPanel>
           <DailyNorma />
