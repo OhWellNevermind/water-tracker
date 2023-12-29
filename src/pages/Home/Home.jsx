91737dee2047e47e7a92cbf46d50c7332eaca9f4
OhWellNevermind <52574266+OhWellNevermind@users.noreply.github.com>
2023-12-29 21:20:49 +0200

    deleted from home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,10 @@
 import { useState } from "react";
 import { colors } from "../../constants";
 
-import { Header } from "../../components/Header/Header";
-import { UserModal } from "../../components/modals/UserModal/UserModal";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { refreshUser } from "../../redux/user/operations";
+import { Container } from "../../components/Container/Container";
 
 export const Home = () => {
   const dispatch = useDispatch();
@@ -13,18 +12,5 @@ export const Home = () => {
     dispatch(refreshUser());
   }, [dispatch]);
 
-  const [open, setOpen] = useState(true);
-  return (
-    <>
-      <Header />
-      <div style={{ fontWeight: 500, fontSize: 32 }}>Home</div>
-      <button
-        style={{ width: "50px", height: "30px", backgroundColor: colors.BLUE }}
-        onClick={() => setOpen(true)}
-      >
-        Open
-      </button>
-      {open && <UserModal setIsOpen={setOpen} />}
-    </>
-  );
+  return <Container>Home</Container>;
 };
