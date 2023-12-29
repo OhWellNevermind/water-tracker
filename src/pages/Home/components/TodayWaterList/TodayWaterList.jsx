0e349dcd1923f41266adea33949bc39e44f4edc2
SePt1k0404 <99293859+SePt1k0404@users.noreply.github.com>
2023-12-29 10:15:17 +0200

    today

--- src/pages/Home/components/TodayWaterList/TodayWaterList.jsx
+++ src/pages/Home/components/TodayWaterList/TodayWaterList.jsx
@@ -103,9 +103,237 @@ export const TodayWaterList = () => {
               </Basket>
             </PencilAndBasket>
           </SublistAll>
+          <SublistAll>
+            <Sublist>
+              <GlassSpan>
+                <Glass width={26} height={26} fill={"#407BFF"} />
+              </GlassSpan>
+              <MLSpan>
+                <NumberMl>{200} ml</NumberMl>
+              </MLSpan>
+              <Time>{"14-00"}</Time>
+            </Sublist>
+            <PencilAndBasket>
+              <Pencil>
+                <PencilSquare width={16} height={16} stroke={"#9EBBFF"} />
+              </Pencil>
+              <Basket>
+                <Trash width={16} height={16} stroke={"#EF5050"} />
+              </Basket>
+            </PencilAndBasket>
+          </SublistAll>
+          <SublistAll>
+            <Sublist>
+              <GlassSpan>
+                <Glass width={26} height={26} fill={"#407BFF"} />
+              </GlassSpan>
+              <MLSpan>
+                <NumberMl>{200} ml</NumberMl>
+              </MLSpan>
+              <Time>{"14-00"}</Time>
+            </Sublist>
+            <PencilAndBasket>
+              <Pencil>
+                <PencilSquare width={16} height={16} stroke={"#9EBBFF"} />
+              </Pencil>
+              <Basket>
+                <Trash width={16} height={16} stroke={"#EF5050"} />
+              </Basket>
+            </PencilAndBasket>
+          </SublistAll>
+          <SublistAll>
+            <Sublist>
+              <GlassSpan>
+                <Glass width={26} height={26} fill={"#407BFF"} />
+              </GlassSpan>
+              <MLSpan>
+                <NumberMl>{200} ml</NumberMl>
+              </MLSpan>
+              <Time>{"14-00"}</Time>
+            </Sublist>
+            <PencilAndBasket>
+              <Pencil>
+                <PencilSquare width={16} height={16} stroke={"#9EBBFF"} />
+              </Pencil>
+              <Basket>
+                <Trash width={16} height={16} stroke={"#EF5050"} />
+              </Basket>
+            </PencilAndBasket>
+          </SublistAll>
+          <SublistAll>
+            <Sublist>
+              <GlassSpan>
+                <Glass width={26} height={26} fill={"#407BFF"} />
+              </GlassSpan>
+              <MLSpan>
+                <NumberMl>{200} ml</NumberMl>
+              </MLSpan>
+              <Time>{"14-00"}</Time>
+            </Sublist>
+            <PencilAndBasket>
+              <Pencil>
+                <PencilSquare width={16} height={16} stroke={"#9EBBFF"} />
+              </Pencil>
+              <Basket>
+                <Trash width={16} height={16} stroke={"#EF5050"} />
+              </Basket>
+            </PencilAndBasket>
+          </SublistAll>
+          <SublistAll>
+            <Sublist>
+              <GlassSpan>
+                <Glass width={26} height={26} fill={"#407BFF"} />
+              </GlassSpan>
+              <MLSpan>
+                <NumberMl>{200} ml</NumberMl>
+              </MLSpan>
+              <Time>{"14-00"}</Time>
+            </Sublist>
+            <PencilAndBasket>
+              <Pencil>
+                <PencilSquare width={16} height={16} stroke={"#9EBBFF"} />
+              </Pencil>
+              <Basket>
+                <Trash width={16} height={16} stroke={"#EF5050"} />
+              </Basket>
+            </PencilAndBasket>
+          </SublistAll>
+          <SublistAll>
+            <Sublist>
+              <GlassSpan>
+                <Glass width={26} height={26} fill={"#407BFF"} />
+              </GlassSpan>
+              <MLSpan>
+                <NumberMl>{200} ml</NumberMl>
+              </MLSpan>
+              <Time>{"14-00"}</Time>
+            </Sublist>
+            <PencilAndBasket>
+              <Pencil>
+                <PencilSquare width={16} height={16} stroke={"#9EBBFF"} />
+              </Pencil>
+              <Basket>
+                <Trash width={16} height={16} stroke={"#EF5050"} />
+              </Basket>
+            </PencilAndBasket>
+          </SublistAll>
+          <SublistAll>
+            <Sublist>
+              <GlassSpan>
+                <Glass width={26} height={26} fill={"#407BFF"} />
+              </GlassSpan>
+              <MLSpan>
+                <NumberMl>{200} ml</NumberMl>
+              </MLSpan>
+              <Time>{"14-00"}</Time>
+            </Sublist>
+            <PencilAndBasket>
+              <Pencil>
+                <PencilSquare width={16} height={16} stroke={"#9EBBFF"} />
+              </Pencil>
+              <Basket>
+                <Trash width={16} height={16} stroke={"#EF5050"} />
+              </Basket>
+            </PencilAndBasket>
+          </SublistAll>
+          <SublistAll>
+            <Sublist>
+              <GlassSpan>
+                <Glass width={26} height={26} fill={"#407BFF"} />
+              </GlassSpan>
+              <MLSpan>
+                <NumberMl>{200} ml</NumberMl>
+              </MLSpan>
+              <Time>{"14-00"}</Time>
+            </Sublist>
+            <PencilAndBasket>
+              <Pencil>
+                <PencilSquare width={16} height={16} stroke={"#9EBBFF"} />
+              </Pencil>
+              <Basket>
+                <Trash width={16} height={16} stroke={"#EF5050"} />
+              </Basket>
+            </PencilAndBasket>
+          </SublistAll>
+          <SublistAll>
+            <Sublist>
+              <GlassSpan>
+                <Glass width={26} height={26} fill={"#407BFF"} />
+              </GlassSpan>
+              <MLSpan>
+                <NumberMl>{200} ml</NumberMl>
+              </MLSpan>
+              <Time>{"14-00"}</Time>
+            </Sublist>
+            <PencilAndBasket>
+              <Pencil>
+                <PencilSquare width={16} height={16} stroke={"#9EBBFF"} />
+              </Pencil>
+              <Basket>
+                <Trash width={16} height={16} stroke={"#EF5050"} />
+              </Basket>
+            </PencilAndBasket>
+          </SublistAll>
+          <SublistAll>
+            <Sublist>
+              <GlassSpan>
+                <Glass width={26} height={26} fill={"#407BFF"} />
+              </GlassSpan>
+              <MLSpan>
+                <NumberMl>{200} ml</NumberMl>
+              </MLSpan>
+              <Time>{"14-00"}</Time>
+            </Sublist>
+            <PencilAndBasket>
+              <Pencil>
+                <PencilSquare width={16} height={16} stroke={"#9EBBFF"} />
+              </Pencil>
+              <Basket>
+                <Trash width={16} height={16} stroke={"#EF5050"} />
+              </Basket>
+            </PencilAndBasket>
+          </SublistAll>
+          <SublistAll>
+            <Sublist>
+              <GlassSpan>
+                <Glass width={26} height={26} fill={"#407BFF"} />
+              </GlassSpan>
+              <MLSpan>
+                <NumberMl>{200} ml</NumberMl>
+              </MLSpan>
+              <Time>{"14-00"}</Time>
+            </Sublist>
+            <PencilAndBasket>
+              <Pencil>
+                <PencilSquare width={16} height={16} stroke={"#9EBBFF"} />
+              </Pencil>
+              <Basket>
+                <Trash width={16} height={16} stroke={"#EF5050"} />
+              </Basket>
+            </PencilAndBasket>
+          </SublistAll>
+          <SublistAll>
+            <Sublist>
+              <GlassSpan>
+                <Glass width={26} height={26} fill={"#407BFF"} />
+              </GlassSpan>
+              <MLSpan>
+                <NumberMl>{200} ml</NumberMl>
+              </MLSpan>
+              <Time>{"14-00"}</Time>
+            </Sublist>
+            <PencilAndBasket>
+              <Pencil>
+                <PencilSquare width={16} height={16} stroke={"#9EBBFF"} />
+              </Pencil>
+              <Basket>
+                <Trash width={16} height={16} stroke={"#EF5050"} />
+              </Basket>
+            </PencilAndBasket>
+          </SublistAll>
         </List>
         <Button type="submit">
-          <PlusSmall width={16} height={16} stroke={"#407BFF"} />
+          <PlusSmall width={12} height={12} stroke={"#407BFF"} />
           <AddWaterButton>Add water</AddWaterButton>
         </Button>
       </TodayWrap>
