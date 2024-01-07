972b51b2185b89e319c195d9b8e161f7a65cc727
OhWellNevermind <52574266+OhWellNevermind@users.noreply.github.com>
2024-01-07 21:37:48 +0200

    fix heigth

--- src/components/modals/UserModal/UserModal.styled.js
+++ src/components/modals/UserModal/UserModal.styled.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 import { colors } from "../../../constants";
 
 export const ModalContainer = styled.div`
-  height: 90vh;
+  max-height: 90vh;
   overflow-y: scroll;
   width: 100%;
   padding: 32px 12px;
