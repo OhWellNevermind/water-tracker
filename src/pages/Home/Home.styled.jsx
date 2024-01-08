248f10c55f9f160644acb5f8a413ec7053f7a732
OhWellNevermind <52574266+OhWellNevermind@users.noreply.github.com>
2024-01-09 00:00:09 +0200

    fix

--- src/pages/Home/Home.styled.jsx
+++ src/pages/Home/Home.styled.jsx
@@ -8,13 +8,11 @@ import BottleHomeDesktop from "/src/images/desktop/BottleHomeDesktop-min.png";
 
 export const ContainerHome = styled.div`
   margin: 0 auto;
-  padding-bottom: 20px;
   @media only screen and (min-width: 768px) {
     width: 706px;
   }
   @media only screen and (min-width: 1440px) {
     width: 1216px;
-    /* padding-top: 20px; */
     display: flex;
     justify-content: center;
     align-items: center;
@@ -35,13 +33,13 @@ export const DailyNormaAndWaterRatioPanel = styled.div`
 export const WrapTodayAndMonth = styled.div`
   margin: 0 auto;
   width: 280px;
-  padding: 24px 8px;
+  padding: 20px 8px;
   background: #ecf2ff;
   border-radius: 10px;
   box-shadow: 0px 4px 14px 0px rgba(64, 123, 255, 0.3);
   @media only screen and (min-width: 768px) {
     width: 704px;
-    padding: 32px 24px;
+    padding: 20px 24px;
   }
   @media only screen and (min-width: 1440px) {
     width: 592px;
