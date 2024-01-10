10c95e5ca097e599b93be5a0c680a4f233048618
SophiaTretiakova <122561236+SophiaTretiakova@users.noreply.github.com>
2024-01-10 15:29:07 +0200

    negative value fix

--- src/components/modals/DailyNorma/DailyNorma.jsx
+++ src/components/modals/DailyNorma/DailyNorma.jsx
@@ -33,13 +33,15 @@ import toast from "react-hot-toast";
 import { updateDailyNorma } from "../../../redux/user/operations";
 
 const schema = Yup.object().shape({
-  weight: Yup.string()
+  weight: Yup.number()
     .min(1, "Please enter valid value")
     .max(10, "Too big value")
+    .positive("Please enter a positive value")
     .required("This field is required"),
-  time: Yup.string()
+  time: Yup.number()
     .min(1, "Please enter valid value")
     .max(10, "Too big value")
+    .positive("Please enter a positive value")
     .required("This field is required"),
   volume: Yup.number()
     .min(0, "Please enter valid value")
@@ -129,8 +131,6 @@ const DailyNorma = ({ onClose }) => {
             values.volume === 0
               ? dispatch(updateDailyNorma(result))
               : dispatch(updateDailyNorma(values.volume));
-            const notify = () => toast("Successfully updated!");
-            notify();
           }}
           validationSchema={schema}
         >
