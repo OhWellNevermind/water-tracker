b9414cfc113738450df3c7087ef5c9c5dc919e18
Seven <graf.lancelot7@gmail.com>
2024-01-10 18:39:53 +0200

    add

--- src/redux/waterTracker/selectors.js
+++ src/redux/waterTracker/selectors.js
@@ -1,3 +1,6 @@
+import { createSelector } from "reselect";
+import { selectDailyNorma } from "../user/selectors";
+
 export const selectWaterTodayDate = (state) => state.waterTracker.today.date;
 
 export const selectIsLoadingWaterTrack = (state) =>
@@ -21,14 +24,16 @@ export const selectTodayWaterData = (state) =>
 export const selectWaterPercentage = (state) =>
   state.waterTracker.today.percentageWaterConsumed;
 
-export const selectDays = (state, days, month, date) => {
-  const trackers = selectWaterMonthTracker(state);
-  const dailyNorma = state.user.user.dailyNorma;
-  const waterTodayDate = selectWaterTodayDate(state).split("-");
-  const day = waterTodayDate.pop();
-  const todayDate = waterTodayDate.join("-");
-  const todayPercentage = selectWaterPercentage(state);
+export const selectMonthTracker = createSelector(
+  [selectWaterTodayTracker],
+  (todayTracker) => {
+    return todayTracker;
+  }
+);
 
+export const selectDays = (state, days, month) => {
+  const trackers = selectWaterMonthTracker(state);
+  const dailyNorma = selectDailyNorma(state);
   const monthDays = [];
   for (let i = 1; i <= days; i += 1) {
     const daySchema = {
@@ -42,26 +47,8 @@ export const selectDays = (state, days, month, date) => {
       return Number(date) === i;
     });
     if (!tracker) {
-      const [itemDay] = daySchema.date.split(",");
-
-      if (Number(itemDay) === Number(day)) {
-        monthDays.push({
-          ...daySchema,
-          percentageWaterConsumed: todayPercentage,
-        });
-      }
       monthDays.push(daySchema);
       continue;
-    }
-    if (date === todayDate) {
-      const [itemDay] = tracker.date.split(",");
-      if (Number(itemDay) === Number(day)) {
-        monthDays.push({
-          ...tracker,
-          percentageWaterConsumed: todayPercentage,
-        });
-        continue;
-      }
     }
     monthDays.push(tracker);
   }
